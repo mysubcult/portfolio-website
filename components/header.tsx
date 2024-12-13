2bd36f931d379b745bd4c7b8a197e6af52699e25
SmartDiag <mysubcult1@gmail.com>
2024-12-13 23:59:47 +0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
@@ -10,31 +10,16 @@ import { useActiveSectionContext } from "@/context/active-section-context";
 export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 transform -translate-x-1/2 h-[4.5rem] w-full max-w-screen-xl rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 transform -translate-x-1/2 h-[4.5rem] w-auto rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       >
-        <nav className="flex h-full items-center justify-between px-5">
-          <div className="sm:hidden text-gray-950 dark:text-gray-200 text-lg font-bold">
-            Logo
-          </div>
-          <div className="sm:hidden">
-            <button onClick={toggleMenu} className="text-gray-500 dark:text-gray-300 focus:outline-none">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-              </svg>
-            </button>
-          </div>
-          <ul className={`sm:flex sm:flex-row sm:items-center sm:justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:gap-5 ${isMenuOpen ? 'flex flex-col' : 'hidden'}`}>
+        <nav className="flex h-full items-center justify-center px-5">
+          <ul className="flex flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:flex-nowrap sm:gap-5">
             {links.map((link) => (
               <motion.li
                 className="h-3/4 flex items-center justify-center relative"
@@ -54,7 +39,6 @@ export default function Header() {
                   onClick={() => {
                     setActiveSection(link.name);
                     setTimeOfLastClick(Date.now());
-                    setIsMenuOpen(false); // Close menu after clicking a link
                   }}
                   style={{
                     whiteSpace: "nowrap",
