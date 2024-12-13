40d07d656aa6235fe97ca20b4d98dee2f8338ea1
SmartDiag <mysubcult1@gmail.com>
2024-12-13 21:45:02 +0400

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,19 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        Приветствую! Я специализируюсь на ремонте ТНВД для автомобилей марки Peugeot, Citroen, BMW и Mini. Моя основная задача — обеспечить надежную и качественную работу вашего двигателя.
       </p>
-
+      <p className="mb-3">
+        Если ваш двигатель троит, глохнет, теряет мощность и динамику разгона, или выдает ошибки Check Engine (например, P0087, P0088, P1336, P2880 и другие), то, к сожалению, ваш ТНВД выходит из строя.
+      </p>
+      <p className="mb-3">
+        Я предлагаю свои услуги по капитальному ремонту ТНВД с гарантией надежности и честности. Включая полную диагностику, модернизированный ремонт и послеремонтную обратную связь. Стоимость услуг составляет 6999 рублей, включая все расходные и уплотнительные материалы.
+      </p>
+      <p className="mb-3">
+        Дополнительно предоставляю фото и видео материалы стадий ремонта и неисправности ТНВД, а также возможность продажи готового насоса или обмена на ваш.
+      </p>
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        Предлагаю сотрудничество с автосервисами на самых выгодных условиях. Доставка осуществляется по всем регионам транспортной компанией или почтой (оплачивается отдельно).
       </p>
     </motion.section>
   );
