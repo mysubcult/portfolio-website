1f184575c35938a1e36a6d04d8d9b764843b2aa6
SmartDiag <mysubcult1@gmail.com>
2024-12-14 17:59:27 +0400

    Update submit-btn.tsx

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaPaperPlane } from "react-icons/fa";
-import { useFormStatus } from "react-dom/experimental";
+import { useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
   const { pending } = useFormStatus();
