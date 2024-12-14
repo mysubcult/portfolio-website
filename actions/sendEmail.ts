02cf9fc043802a2ccabea0d4f5f46ccec63bca09
SmartDiag <mysubcult1@gmail.com>
2024-12-14 17:56:13 +0400

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -29,7 +29,7 @@ export const sendEmail = async (formData: FormData) => {
       from: "Contact Form <onboarding@resend.dev>",
       to: "mysubcult1@gmail.com",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      replyTo: senderEmail, // Исправьте на replyTo
       react: React.createElement(ContactFormEmail, {
         message: message,
         senderEmail: senderEmail,
