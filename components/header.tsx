c52f67c9e4a6378d9137c0a26315a66407ba797e
SmartDiag <mysubcult1@gmail.com>
2024-12-17 21:27:25 +0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
@@ -10,184 +10,56 @@ import { useActiveSectionContext } from "@/context/active-section-context";
 export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMobileMenu, setIsMobileMenu] = useState(false);
-  const navRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const checkMenuVisibility = () => {
-      if (navRef.current) {
-        const items = navRef.current.querySelectorAll("li");
-        let isOverflowing = false;
-
-        items.forEach((item) => {
-          const rect = item.getBoundingClientRect();
-          if (rect.right > window.innerWidth) {
-            isOverflowing = true;
-          }
-        });
-
-        setIsMobileMenu(isOverflowing);
-      }
-    };
-
-    const resizeObserver = new ResizeObserver(() => {
-      checkMenuVisibility();
-    });
-
-    if (navRef.current) {
-      resizeObserver.observe(navRef.current);
-    }
-
-    checkMenuVisibility(); // Initial check
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, []);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-auto w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-auto sm:w-auto sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
-      >
-        <nav ref={navRef} className="flex justify-between items-center py-4 sm:py-0">
-          {isMobileMenu ? (
-            <div className="flex justify-end w-full">
-              <button
-                onClick={toggleMenu}
-                className="text-gray-500 hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300 focus:outline-none"
-              >
-                <svg
-                  className="w-8 h-8"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16m-7 6h7"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-          ) : (
-            <ul className="flex items-center justify-center gap-y-1 text-[1.1rem] font-medium text-gray-500 sm:gap-6">
-              {links.map((link) => (
-                <motion.li
-                  className="h-auto flex items-center justify-center relative whitespace-nowrap"
-                  key={link.hash}
-                  initial={{ y: -100, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                >
-                  <Link
-                    className={clsx(
-                      "flex items-center justify-center px-4 py-4 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
-                      {
-                        "text-gray-950 dark:text-gray-200":
-                          activeSection === link.name,
-                      }
-                    )}
-                    href={link.hash}
-                    onClick={() => {
-                      setActiveSection(link.name);
-                      setTimeOfLastClick(Date.now());
-                    }}
-                  >
-                    {link.name}
+      ></motion.div>
 
-                    {link.name === activeSection && (
-                      <motion.span
-                        className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                        layoutId="activeSection"
-                        transition={{
-                          type: "spring",
-                          stiffness: 380,
-                          damping: 30,
-                        }}
-                      ></motion.span>
-                    )}
-                  </Link>
-                </motion.li>
-              ))}
-            </ul>
-          )}
-        </nav>
-      </motion.div>
-      {isMenuOpen && (
-        <div className="fixed inset-0 bg-white dark:bg-gray-950 z-50 sm:hidden">
-          <div className="flex justify-end p-4">
-            <button
-              onClick={toggleMenu}
-              className="text-gray-500 hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300 focus:outline-none"
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
-              <svg
-                className="w-8 h-8"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  {
+                    "text-gray-950 dark:text-gray-200":
+                      activeSection === link.name,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <ul className="flex flex-col items-center justify-center gap-y-6 text-[1.1rem] font-medium text-gray-500">
-            {links.map((link) => (
-              <motion.li
-                className="h-auto flex items-center justify-center relative whitespace-nowrap"
-                key={link.hash}
-                initial={{ y: -100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-              >
-                <Link
-                  className={clsx(
-                    "flex items-center justify-center px-4 py-4 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
-                    {
-                      "text-gray-950 dark:text-gray-200":
-                        activeSection === link.name,
-                    }
-                  )}
-                  href={link.hash}
-                  onClick={() => {
-                    setActiveSection(link.name);
-                    setTimeOfLastClick(Date.now());
-                    setIsMenuOpen(false); // Close the menu after clicking a link
-                  }}
-                >
-                  {link.name}
+                {link.name}
 
-                  {link.name === activeSection && (
-                    <motion.span
-                      className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                      layoutId="activeSection"
-                      transition={{
-                        type: "spring",
-                        stiffness: 380,
-                        damping: 30,
-                      }}
-                    ></motion.span>
-                  )}
-                </Link>
-              </motion.li>
-            ))}
-          </ul>
-        </div>
-      )}
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
