a1e4d72ee624f25aeb569b024a0d6342c9975354
SmartDiag <mysubcult1@gmail.com>
2024-12-18 18:29:00 +0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -20,17 +20,17 @@ export default function Header() {
       ></motion.div>
 
       <nav className="flex fixed top-[0.2rem] left-1/2 h-14 -translate-x-1/2 py-2 sm:top-[1.8rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-full max-w-[43rem] flex-wrap items-center justify-center gap-y-1 text-[1.1rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-6">
+        <ul className="flex flex-wrap items-center justify-center gap-y-1 text-[1.1rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-6">
           {links.map((link) => (
             <motion.li
-              className="h-3/4 flex items-center justify-center relative"
+              className="h-3/4 flex items-center justify-center relative w-full sm:w-auto"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className={clsx(
-                  "flex items-center justify-center px-4 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "flex items-center justify-center px-4 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 whitespace-nowrap",
                   {
                     "text-gray-950 dark:text-gray-200":
                       activeSection === link.name,
