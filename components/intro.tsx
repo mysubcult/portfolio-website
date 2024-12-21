aaf0a2e9ff3f2323a5a8789ceaac7b0174144be6
SmartDiag <mysubcult1@gmail.com>
2024-12-22 01:44:55 +0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -72,56 +72,52 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <div className="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
-          <Link
-            href="#contact"
-            className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-            onClick={() => {
-              setActiveSection("Обратная связь");
-              setTimeOfLastClick(Date.now());
-            }}
-          >
-            Связаться со мной{" "}
-            <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-          </Link>
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Обратная связь");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Связаться со мной{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
 
-          <Link
-            href="#projects"
-            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-            onClick={() => {
-              setActiveSection("Услуги");
-              setTimeOfLastClick(Date.now());
-            }}
-          >
-            Услуги{" "}
-          </Link>
-        </div>
+        <Link
+          href="#projects"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          onClick={() => {
+            setActiveSection("Услуги");
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          Услуги{" "}
+        </Link>
 
-        <div className="flex flex-col sm:flex-row gap-2 w-full sm:w-auto mt-4 sm:mt-0">
-          <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://vk.com"
-            target="_blank"
-          >
-            <FaVk />
-          </a>
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://vk.com"
+          target="_blank"
+        >
+          <FaVk />
+        </a>
 
-          <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://telegram.org"
-            target="_blank"
-          >
-            <FaTelegramPlane />
-          </a>
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://telegram.org"
+          target="_blank"
+        >
+          <FaTelegramPlane />
+        </a>
 
-          <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://whatsapp.com"
-            target="_blank"
-          >
-            <FaWhatsapp />
-          </a>
-        </div>
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://whatsapp.com"
+          target="_blank"
+        >
+          <FaWhatsapp />
+        </a>
       </motion.div>
     </section>
   );
