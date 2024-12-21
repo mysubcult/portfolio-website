524e4c9f1e1ef8c0485c91bc6ad5a7a740aa95f2
SmartDiag <mysubcult1@gmail.com>
2024-12-22 01:24:48 +0400

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -17,7 +17,7 @@ export default function Experience() {
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
-      <SectionHeading>Квалификация</SectionHeading>
+      <SectionHeading>🎓 Квалификация</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
