ee138c823927b08d34c045460170a2ef7a79f727
SmartDiag <mysubcult1@gmail.com>
2024-12-22 01:21:30 +0400

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>Цены</SectionHeading>
+      <SectionHeading>💰 Цены</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
