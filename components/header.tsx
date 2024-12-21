e776a4ed74d55756c8ba3c9725a3e404c39917b3
SmartDiag <mysubcult1@gmail.com>
2024-12-22 01:36:42 +0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ export default function Header() {
 
       <nav className="flex fixed top-[0.2rem] left-1/2 h-14 -translate-x-1/2 py-2 sm:top-[1.8rem] sm:h-[initial] sm:py-0">
         <button
-          className="sm:hidden text-gray-500 focus:outline-none"
+          className="sm:hidden text-gray-500 focus:outline-none absolute left-4 top-1/2 transform -translate-y-1/2"
           onClick={toggleMobileMenu}
         >
           <svg
@@ -42,13 +42,13 @@ export default function Header() {
           </svg>
         </button>
         <ul
-          className={`flex flex-wrap items-center justify-center gap-y-1 text-[1.1rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-6 ${
-            isMobileMenuOpen ? "block" : "hidden"
-          } sm:flex`}
+          className={`flex flex-col items-center justify-center gap-y-4 text-[1.1rem] font-medium text-gray-500 fixed inset-0 bg-white bg-opacity-90 backdrop-blur-sm z-50 transform transition-transform duration-300 ${
+            isMobileMenuOpen ? "translate-y-0" : "-translate-y-full"
+          } sm:hidden`}
         >
           {links.map((link) => (
             <motion.li
-              className="h-3/4 flex items-center justify-center relative w-auto sm:w-auto"
+              className="h-3/4 flex items-center justify-center relative w-auto"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
