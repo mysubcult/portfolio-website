79daae5233e1e230375300879514a2eff2c2c8d9
SmartDiag <mysubcult1@gmail.com>
2024-12-22 18:31:18 +0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -88,11 +88,11 @@ export default function Intro() {
           href="#projects"
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           onClick={() => {
-            setActiveSection("Услуги");
+            setActiveSection("Цены");
             setTimeOfLastClick(Date.now());
           }}
         >
-          Услуги{" "}
+          Цены{" "}
         </Link>
 
         <div className="flex flex-row sm:flex-row gap-2">
