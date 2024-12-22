e53421c672cf582322d88e3365921fb204788eab
SmartDiag <mysubcult1@gmail.com>
2024-12-22 18:09:34 +0400

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -24,7 +24,7 @@ export default function Contact() {
         <div className="w-full sm:w-1/2 p-4">
           <h2 className="text-lg font-semibold">📞 Контакты</h2>
           <h3 className="text-base font-semibold">🌐 Мы в социальных сетях:</h3>
-          <div className="flex flex-col sm:flex-row items-center sm:items-start mt-4 space-x-4">
+          <div className="flex flex-col sm:flex-row items-center sm:items-start mt-4 space-x-4 justify-center">
             <motion.div
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
@@ -88,17 +88,6 @@ export default function Contact() {
             <p className="text-gray-700 dark:text-white/80">
               1234 Улица, Город, Страна
             </p>
-            <div className="mt-4">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434501749!2d30.52333331546757!3d50.4501009794921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4f4571bf51%3A0x764931d2170146fe!2z0JrQuNC90YLQtdC90LjRjw!5e0!3m2!1sru!2sua!4v1633024167757!5m2!1sru!2sua"
-                width="100%"
-                height="300"
-                style={{ border: 0 }}
-                allowFullScreen={true}
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </div>
           </div>
         </div>
         <div className="w-full sm:w-1/2 p-4">
@@ -141,6 +130,18 @@ export default function Contact() {
             <SubmitBtn />
           </form>
         </div>
+      </div>
+      <div className="w-full mt-8">
+        <h2 className="text-lg font-semibold mb-4">📍 Мы на карте</h2>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434501749!2d30.52333331546757!3d50.4501009794921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4f4571bf51%3A0x764931d2170146fe!2z0JrQuNC90YLQtdC90LjRjw!5e0!3m2!1sru!2sua!4v1633024167757!5m2!1sru!2sua"
+          width="100%"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </section>
   );
