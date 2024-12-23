c77c646afab8ea30a78069afe9c69556c3874366
SmartDiag <mysubcult1@gmail.com>
2024-12-23 19:42:36 +0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -74,30 +74,32 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-8 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection("Обратная связь");
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          Связаться со мной{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
+        <div className="flex flex-col sm:flex-row gap-2 w-full">
+          <Link
+            href="#contact"
+            className="group bg-gray-900 text-white px-8 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition w-full sm:w-auto"
+            onClick={() => {
+              setActiveSection("Обратная связь");
+              setTimeOfLastClick(Date.now());
+            }}
+          >
+            Связаться со мной{" "}
+            <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          </Link>
 
-        <Link
-          href="#prices"
-          className="group bg-white px-8 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          onClick={() => {
-            setActiveSection("Цены");
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          Цены{" "}
-        </Link>
+          <Link
+            href="#prices"
+            className="group bg-white px-8 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 w-full sm:w-auto"
+            onClick={() => {
+              setActiveSection("Цены");
+              setTimeOfLastClick(Date.now());
+            }}
+          >
+            Цены{" "}
+          </Link>
+        </div>
 
-        <div className="flex flex-row sm:flex-row gap-2">
+        <div className="flex flex-row sm:flex-row gap-2 w-full mt-4 sm:mt-0">
           <a
             className="bg-white p-5 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://vk.com/evdokimovrus"
