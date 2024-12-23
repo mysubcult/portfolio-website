a9dc188284450f6df5c3bb766c35e4527ceef47a
SmartDiag <mysubcult1@gmail.com>
2024-12-24 01:38:50 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,23 @@ import Intro from "@/components/intro";
 import Prices from "@/components/prices";
 import SectionDivider from "@/components/section-divider";
 import Services from "@/components/services";
+import Head from "next/head"; // Импортируем Head
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">
-      <Intro />
-      <SectionDivider />
-      <About />
-      <Prices />
-      <Services />
-      <Experience />
-      <Contact />
-    </main>
+    <>
+      <Head>
+        <meta name="yandex-verification" content="3bf07cdf9c04392c" />
+      </Head>
+      <main className="flex flex-col items-center px-4">
+        <Intro />
+        <SectionDivider />
+        <About />
+        <Prices />
+        <Services />
+        <Experience />
+        <Contact />
+      </main>
+    </>
   );
 }
