aa30250e7e8851c70883f9398714ed44b525f2bf
SmartDiag <mysubcult1@gmail.com>
2025-01-15 02:16:25 +0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -15,17 +15,17 @@ const Header = memo(function Header() {
     <header className="z-[999] relative">
       <motion.div
         className="
-          fixed top-0 left-1/2 h-[5.2rem] w-full rounded-none border border-white border-opacity-40 
+          fixed top-0 left-1/2 h-[clamp(4rem, 10vh, 5.2rem)] w-full rounded-none border border-white border-opacity-40 
           bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] 
-          sm:top-6 sm:h-[3.6rem] sm:w-[48rem] sm:rounded-full 
+          sm:top-6 sm:h-[3.6rem] sm:w-[90%] sm:max-w-[48rem] sm:min-w-[20rem] sm:rounded-full 
           dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75
         "
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
-      <nav className="flex fixed top-[0.2rem] left-1/2 h-14 -translate-x-1/2 py-2 sm:top-[1.8rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[24rem] flex-wrap items-center justify-center gap-y-1 text-[1.1rem] font-semibold text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <nav className="flex fixed top-[0.2rem] left-1/2 h-14 -translate-x-1/2 py-2 sm:top-[1.8rem] sm:h-[initial] sm:py-0 overflow-x-auto">
+        <ul className="flex w-[24rem] flex-wrap items-center justify-center gap-y-1 text-[clamp(0.9rem, 2vw, 1.1rem)] font-semibold text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-[2%]">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
